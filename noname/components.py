e22718be3cb852fb3244e65fb9d678127f65ac23
Matiiss <83066658+Matiiss@users.noreply.github.com>
2022-06-09 15:33:21 +0300

    Added new attributes to Position and created FRect: rectangle that stores float values

--- noname/components.py
+++ noname/components.py
@@ -1,8 +1,22 @@
-import pygame
+from .settings import *
 
 
 class Position(pygame.Vector2):
-    pass
+    @property
+    def cx(self) -> int:
+        return int(self.x) // TILE_SIZE
+
+    @property
+    def cy(self) -> int:
+        return int(self.y) // TILE_SIZE
+
+    @property
+    def xr(self) -> float:
+        return (self.x - self.cx * TILE_SIZE) / TILE_SIZE
+
+    @property
+    def yr(self) -> float:
+        return (self.y - self.cy * TILE_SIZE) / TILE_SIZE
 
 
 class Velocity(pygame.Vector2):
@@ -22,3 +36,33 @@ class Rectangle(pygame.Rect):
 
 class Tile(pygame.Rect):
     pass
+
+
+class FRect:
+    def __init__(self, x: float = 0, y: float = 0, width: float = 0, height: float = 0):
+        self.x, self.y = x, y
+        self.width, self.height = width, height
+
+    def move(self, x_change, y_change):
+        return FRect(self.x + x_change, self.y + y_change, self.width, self.height)
+
+    def colliderect(self, other):
+        return (
+            self.x < other.x + other.width
+            and self.x + self.width > other.x
+            and self.y < other.y + other.height
+            and self.y + self.height > other.y
+        )
+
+    @property
+    def center(self):
+        return self.x + self.width / 2, self.y + self.height / 2
+
+    @center.setter
+    def center(self, value: tuple[int, int] | pygame.Vector2):
+        x, y = value
+        self.x = x - self.width / 2
+        self.y = y - self.height / 2
+
+    def __str__(self):
+        return f"<{self.__class__.__name__}({self.x, self.y, self.width, self.height})>"
