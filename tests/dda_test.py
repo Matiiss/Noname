8faec462d0f6f6ab928d277d6b114b8a551dd57a
Matiiss <83066658+Matiiss@users.noreply.github.com>
2022-06-14 01:18:50 +0300

    Testing dda

--- tests/dda_test.py
+++ tests/dda_test.py
@@ -2,12 +2,11 @@ import math
 
 import dda
 
-collision_map = [
-    [0, 0, 1, 1],
-    [0, 0, 0, 1],
-    [0, 0, 0, 1],
-    [0, 0, 0, 1]
-]
+collision_map = [[0, 0, 1, 1], [0, 0, 0, 1], [0, 0, 0, 1], [0, 0, 0, 1]]
 
 print(dda.from_angle(32, 32, math.radians(180), collision_map, 16))
-print(dda.from_angle_range(32, 32, math.radians(10), math.radians(80), math.radians(1), collision_map, 16))
+print(
+    dda.from_angle_range(
+        32, 32, math.radians(10), math.radians(80), math.radians(1), collision_map, 16
+    )
+)
