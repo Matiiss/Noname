cf9867e567943f4a872a599be0e0da0598229856
Matiiss <83066658+Matiiss@users.noreply.github.com>
2022-06-17 07:06:10 +0300

    Updated tests with the new max_distance argument

--- tests/dda_test.py
+++ tests/dda_test.py
@@ -4,9 +4,16 @@ import dda
 
 collision_map = [[0, 0, 1, 1], [0, 0, 0, 1], [0, 0, 0, 1], [0, 0, 0, 1]]
 
-print(dda.from_angle(32, 32, math.radians(180), collision_map, 16))
+print(dda.from_angle(32, 32, math.radians(180), 185, collision_map, 16))
 print(
     dda.from_angle_range(
-        32, 32, math.radians(10), math.radians(80), math.radians(1), collision_map, 16
+        32,
+        32,
+        math.radians(10),
+        math.radians(80),
+        math.radians(1),
+        185,
+        collision_map,
+        16,
     )
 )
