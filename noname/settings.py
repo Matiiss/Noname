5305789c856f8baf0bd1c632b93ec39a1795f918
Matiiss <83066658+Matiiss@users.noreply.github.com>
2022-06-17 07:02:14 +0300

    Changed tile size back to 16, added data used by came to be able to cap its values, added max_distance, which could be dynamic later.

--- noname/settings.py
+++ noname/settings.py
@@ -1,6 +1,10 @@
+import math
+
 import pygame
 
 # display settings
+# WIDTH, HEIGHT = 160, 90
+# WIDTH, HEIGHT = 192, 108
 WIDTH, HEIGHT = 320, 180
 # WIDTH, HEIGHT = 640, 360
 DISPLAY_FLAGS = pygame.SCALED
@@ -10,4 +14,10 @@ FPS = 60
 FRAME_CONSTANT = 60
 
 # tile settings
-TILE_SIZE = 32
+TILE_SIZE = 16
+
+# map settings (info more like)
+MAP_WIDTH, MAP_HEIGHT = 40, 20
+
+# shadow settings
+MAX_RAY_DISTANCE = math.ceil(math.sqrt(WIDTH ** 2 + HEIGHT ** 2))
